a013b9f7e4a6ca5dee913b9d28dce393f27960da
Robel Roba <robel.roba@aastustudent.edu.et>
2025-08-24 17:47:12 +0300

    Time: 0 ms (100.00%) | Memory: 12.5 MB (26.50%) - LeetSync

---
+++ 50-powx-n/powx-n.py
@@ -0,0 +1,17 @@
+class Solution:
+    def myPow(self, x, n):
+        def pow(x, n):
+            if n == 0:
+                return 1
+
+            half = pow(x, n // 2)
+
+            if n % 2:
+                return x * half * half
+            else:
+                return half * half
+        
+        if n > 0:
+            return pow(x, n)
+        else:
+            return 1 / pow(x, abs(n))
