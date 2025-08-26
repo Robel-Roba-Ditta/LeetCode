30f93c809cb085d935ffe209f3ae413530890025
Robel Roba <robel.roba@aastustudent.edu.et>
2025-08-26 19:29:48 +0300

    Time: 2 ms (69.20%) | Memory: 15 MB (66.88%) - LeetSync

---
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+class Solution:
+    def maxDepth(self, root):
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+class Solution:
+    def maxDepth(self, root):
+        stack = [[root, 1]]
+        res = 0
+
+        while stack:
+            node, depth = stack.pop()
+            if node:
+                res = max(res, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+        return res
+
+class Solution:
+    def maxDepth(self, root):
+        q = deque()
+        if root:
+            q.append(root)
+        level = 0
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level += 1
+        return level
