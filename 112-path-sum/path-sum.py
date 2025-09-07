5baabb4f3428a7ad000d177c6764b71975c25957
Robel Roba <robel.roba@aastustudent.edu.et>
2025-09-07 09:05:51 +0300

    Time: 0 ms (100.00%) | Memory: 14.2 MB (62.51%) - LeetSync

--- 112-path-sum/path-sum.py
+++ 112-path-sum/path-sum.py
@@ -11,15 +11,13 @@ class Solution:
     def hasPathSum(self, root, sum):
         if not root:
             return False
-        de = [
-            (root, sum - root.val),
-        ]
-        while de:
-            node, curr_sum = de.pop()
+        stack = [(root, sum - root.val)]
+        while stack:
+            node, curr_sum = stack.pop()
             if not node.left and not node.right and curr_sum == 0:
                 return True
             if node.right:
-                de.append((node.right, curr_sum - node.right.val))
+                stack.append((node.right, curr_sum - node.right.val))
             if node.left:
-                de.append((node.left, curr_sum - node.left.val))
+                stack.append((node.left, curr_sum - node.left.val))
         return False
