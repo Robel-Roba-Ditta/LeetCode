20f61222b45233b148fc0b92099134c27fca378a
Robel Roba <robel.roba@aastustudent.edu.et>
2025-09-11 18:44:36 +0300

    Time: 133 ms (20.11%) | Memory: 39.4 MB (7.47%) - LeetSync

---
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
@@ -0,0 +1,35 @@
+class TrieNode:
+    def __init__(self):
+        self.children = [None] * 26
+        self.end = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        curr = self.root
+        for c in word:
+            i = ord(c) - ord("a")
+            if curr.children[i] is None:
+                curr.children[i] = TrieNode()
+            curr = curr.children[i]
+        curr.end = True
+
+    def search(self, word):
+        curr = self.root
+        for c in word:
+            i = ord(c) - ord("a")
+            if curr.children[i] is None:
+                return False
+            curr = curr.children[i]
+        return curr.end
+
+    def startsWith(self, prefix):
+        curr = self.root
+        for c in prefix:
+            i = ord(c) - ord("a")
+            if curr.children[i] is None:
+                return False
+            curr = curr.children[i]
+        return True
