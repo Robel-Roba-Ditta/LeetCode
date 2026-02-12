2cb6b43a5c3cc1a28b04c0d705dab660f888df32
Robel Roba <robel.roba@aastustudent.edu.et>
2026-02-12 15:25:36 +0300

    Time: 78 ms (89.42%) | Memory: 20.7 MB (13.45%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
@@ -3,8 +3,8 @@ class Solution:
         c1 = Counter(word1)
         c2 = Counter(word2)
 
-        if len(word1) != len(word2):
-            return False
+        # if len(word1) != len(word2):
+        #     return False
 
         if set(c1.keys()) != set(c2.keys()):
             return False
