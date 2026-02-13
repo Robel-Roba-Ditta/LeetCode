a8763d78e5bdbfbbd26bae7fe817f3da2cc22df0
Robel Roba <robel.roba@aastustudent.edu.et>
2026-02-13 15:39:58 +0300

    Time: 3 ms (12.66%) | Memory: 19.4 MB (18.44%) - LeetSync

--- 861-flipping-an-image/flipping-an-image.py
+++ 861-flipping-an-image/flipping-an-image.py
@@ -3,7 +3,7 @@ class Solution:
         new = [] 
         for i in image:
             i.reverse()
-            inverted_list = [1 if x == 0 else 0 for x in i] 
-            new.append(inverted_list)
+            k = [1 if x == 0 else 0 for x in i] 
+            new.append(k)
         return new
         
