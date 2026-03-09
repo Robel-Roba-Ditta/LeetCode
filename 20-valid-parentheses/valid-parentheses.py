5c93caeb17fe52e527c10a1c2ce23581870d7e60
Robel Roba <robel.roba@aastustudent.edu.et>
2026-03-09 16:45:07 +0300

    Time: 0 ms (100.00%) | Memory: 19.1 MB (99.78%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        par = { ']' :'[' ,  ')' : '(', '}' : '{' }
+
+        stack = []
+        for c in s:
+            if c in par:
+                if len(stack) == 0 or stack[-1] != par[c]:
+                    return False
+                else:
+                    stack.pop()
+            else:
+                stack.append(c)
+        
+        return len(stack) == 0
+
+        
