eea349d663da025352b6cb80372bce54b015716f
Robel Roba <robel.roba@aastustudent.edu.et>
2026-03-11 16:33:17 +0300

    Time: 25 ms (47.47%) | Memory: 25.7 MB (52.37%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,5 +1,5 @@
 class Solution:
-    def productExceptSelf(self, nums):
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
         res = [1] * (len(nums))
 
         for i in range(1, len(nums)):
@@ -9,3 +9,4 @@ class Solution:
             res[i] *= postfix
             postfix *= nums[i]
         return res
+        
