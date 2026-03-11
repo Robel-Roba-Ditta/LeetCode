5963ebdb3a7c358cb64ca8d836c7db54706ea8cc
Robel Roba <robel.roba@aastustudent.edu.et>
2026-03-11 19:32:11 +0300

    Time: 47 ms (37.99%) | Memory: 22 MB (48.99%) - LeetSync

---
+++ 1582-design-browser-history/design-browser-history.py
@@ -0,0 +1,27 @@
+class LinkedList:
+    def __init__(self, node = None, next = None, prev = None):
+        self.node = node
+        self.next = next
+        self.prev = prev
+
+class BrowserHistory:
+
+    def __init__(self, homepage: str):
+        self.current = LinkedList(homepage)
+
+    def visit(self, url: str) -> None:
+        self.current.next = LinkedList(url)
+        self.current.next.prev = self.current
+        self.current = self.current.next
+
+    def back(self, steps: int) -> str:
+        while self.current.prev and steps > 0:
+            self.current = self.current.prev
+            steps -= 1
+        return self.current.node
+
+    def forward(self, steps: int) -> str:
+        while self.current.next and steps > 0:
+            self.current = self.current.next
+            steps -= 1
+        return self.current.node
