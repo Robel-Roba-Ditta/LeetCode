386ab1de1a9c01837336dce29cb125777cda45f3
Robel Roba <robel.roba@aastustudent.edu.et>
2026-03-13 16:15:28 +0300

    Time: 191 ms (54.03%) | Memory: 34.2 MB (98.71%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -1,5 +1,5 @@
 class Solution:
-    def maxSlidingWindow(self, nums, k):
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         output = []
         q = collections.deque()
         l = r = 0
