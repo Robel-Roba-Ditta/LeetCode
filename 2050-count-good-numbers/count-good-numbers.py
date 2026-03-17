8e09459dd2d444203925449a5413103970da984f
Robel Roba <robel.roba@aastustudent.edu.et>
2026-03-17 19:37:47 +0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (84.60%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.py
+++ 2050-count-good-numbers/count-good-numbers.py
@@ -4,11 +4,13 @@ class Solution:
         def power(x, n):
             if n == 0:
                 return 1
-            left = power(x, n // 2)
-            if n % 2 == 0:
-                return (left * left) % MOD
-            else:
-                return (x * left * left) % MOD
+            res = 1
+            while n > 1:
+                if n % 2:
+                    res = (res * x) % MOD
+                n = n // 2
+                x = (x ** 2) % MOD
+            return res * x
         even = (n + 1) // 2
         odd = n // 2
         return (power(5, even) * power(4, odd)) % MOD
