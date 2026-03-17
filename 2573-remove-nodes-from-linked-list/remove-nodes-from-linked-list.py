dce2886b68aa2909403ee5d383ed27eb8d9e390c
Robel Roba <robel.roba@aastustudent.edu.et>
2026-03-17 17:24:40 +0300

    Time: 144 ms (20.11%) | Memory: 74.9 MB (7.15%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.py
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.py
@@ -1,15 +1,11 @@
 class Solution:
-    def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        cur = head
-        stack = []
-        while cur:
-            while stack and stack[-1].val < cur.val:
-                stack.pop()
-            stack.append(cur)
-            cur = cur.next
-        for i in range(len(stack) - 1):
-            stack[i].next = stack[i+1]
-        stack[-1].next = None
-        return stack[0]
-
-        
+    def removeNodes(self, head: ListNode) -> ListNode:
+        def helper(node: ListNode) -> ListNode:
+            if not node:
+                return None
+            node.next = helper(node.next)
+            if node.next and node.val < node.next.val:
+                return node.next
+            else:
+                return node
+        return helper(head)
