beab9bc401edd3005ea3f66af5fd2218f2d26834
Robel Roba <robel.roba@aastustudent.edu.et>
2026-03-25 17:20:57 +0300

    Time: 107 ms (67.97%) | Memory: 61.2 MB (90.76%) - LeetSync

--- 77-combinations/combinations.py
+++ 77-combinations/combinations.py
@@ -1,13 +1,13 @@
 class Solution:
-    def combine(self, n, k):
+    def combine(self, n: int, k: int) -> List[List[int]]:
         res = []
-        def helper(start, comb):
-            if len(comb) == k:
-                res.append(comb[:])
+        def backtrack(start, path):
+            if len(path) == k:
+                res.append(path[:])
                 return
             for i in range(start, n+1):
-                comb.append(i)
-                helper(i+1, comb)
-                comb.pop()
-        helper(1, [])
+                path.append(i)
+                backtrack(i+1, path)
+                path.pop()
+        backtrack(1, [])
         return res
